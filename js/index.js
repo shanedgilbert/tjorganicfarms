a64d4d5d388a6ba5a4c8ee92466bc661ad24e458
Shane Gilbert <shanedgilbert@gmail.com>
2019-09-12 22:55:15 +0800

    updated and redesigned

---
+++ js/index.js
@@ -0,0 +1,49 @@
+window.onload = function() {
+  // JavaScript for scroll to top button
+  const scrollToTopButton = document.getElementById('scroll-to-top');
+  const scrollToBottomButton = document.getElementById('scroll-to-bottom');
+
+  //Shows up arrow when scroll > 200 px
+  const scrollFuncTop = () => {
+    // Get the current scroll value
+    let y = window.scrollY;
+
+    // If the scroll value is greater than the window height, let's add a class to the scroll-to-top button to show it!
+    if (y > 200) {
+      scrollToTopButton.className = "scroll-top show";
+    } else {
+      scrollToTopButton.className = "scroll-top hide";
+    }
+  };
+
+  //Shows down arrow when scroll <= 200px
+  const scrollFuncBot = () => {
+    // Get the current scroll value
+    let y = window.scrollY;
+
+    // If the scroll value is greater than the window height, let's add a class to the scroll-to-top button to show it!
+    if (y <= 200) {
+      scrollToBottomButton.className = "scroll-bot show";
+    } else {
+      scrollToBottomButton.className = "scroll-bot hide";
+    }
+  };
+  window.addEventListener("scroll", scrollFuncTop);
+  window.addEventListener("scroll", scrollFuncBot);
+
+  //Scroll to top
+  $('.scroll-top').click(function() {
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') )
+    }, 0);
+     return false;
+  });
+
+  //Scroll to below main picture
+  $('.scroll-bot').click(function() {
+    $('html, body').animate({
+        scrollTop: $("#content").offset().top
+    }, 0);
+     return false;
+  });
+};
